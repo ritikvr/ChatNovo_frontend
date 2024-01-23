3a4e49b6b76ed294d2d1d852f3a8ddd9225b593c
Ritik <ritik11oct2003@gmail.com>
2024-01-23 19:49:40 +0530

    2 commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import axios from "axios";
 import "./index.css";
 import App from "./App";
-axios.defaults.baseURL = "https://genie-bazaar-backend.vercel.app";
+axios.defaults.baseURL = "https://chatnovo-backend.vercel.app/";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
